a0889dd5abb24f8be7c6bff9c1bd3434e9382bf1
matt-pilcher <mpilcher90@gmail.com>
2023-03-10 19:25:04 -0500

    add two backend endpoints to run predictions on Replicate

---
+++ src/pages/api/predictions/index.js
@@ -0,0 +1,29 @@
+ export default async function handler(req, res) {
+  const image = req.body.image;
+  const response = await fetch("https://api.replicate.com/v1/predictions", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // The machine learning models on Replicate are version controlled for stability
+      version: "fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",
+      // The image that will be submitted by the user on the frontend
+      input: { 
+        image, 
+      },
+    }),
+  });
+
+  if (response.status !== 201) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+}
