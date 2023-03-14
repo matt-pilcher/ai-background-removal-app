58ac9f82a97591a3ff9984fa159dd57a020ccff2
matt-pilcher <mpilcher90@gmail.com>
2023-03-14 17:40:37 -0400

    add image upload functionality

---
+++ components/UploadWrapper.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function UploadWrapper({ uploadButtonElement }) {
+  return (
+    <section className="relative">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          <div className="text-center pb-12 md:pb-16">
+            <h1
+              className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              AI-Powered Background Image Removal
+            </h1>
+            <div className="max-w-3xl mx-auto">
+              <p
+                className="text-xl text-gray-600 mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                Remove the background from an uploaded image asset
+              </p>
+              <div
+                className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+                data-aos="zoom-y-out"
+                data-aos-delay="300"
+              >
+                {uploadButtonElement("Upload")}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default UploadWrapper;
