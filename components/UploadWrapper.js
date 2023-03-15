b037ca286fcbd4803506f6f3b79b89129e216d40
matt-pilcher <mpilcher90@gmail.com>
2023-03-15 19:06:08 -0400

    add initial font scaling/styles and responsive layout

--- components/UploadWrapper.js
+++ components/UploadWrapper.js
@@ -3,13 +3,15 @@ import Image from "next/image";
 
 function UploadWrapper({ uploadButtonElement }) {
   return (
-    <section className="flex mx-auto justify-between max-w-7xl py-6">
-      <div className="justify-center flex flex-col gap-5">
-        <h1>AI-Powered Background Image Removal</h1>
-        <p>Remove the background from an uploaded image asset</p>
-        {uploadButtonElement("Upload")}
+    <section className="flex mx-auto flex-col md:flex-row justify-between max-w-7xl p-6">
+      <div className="justify-center flex flex-col md:w-1/2">
+        <h1 className="font-bold leading-tight text-gray-900 lg:text-6xl md:text-5xl text-3xl mb-5">AI-Powered Background Image Removal</h1>
+        <p className="font-medium mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        <div className="my-4">
+          {uploadButtonElement("Upload Image")}
+        </div>
       </div>
-      <div>
+      <div className="flex md:w-1/3">
         <Image width={400} height={600} src='/bg-step1.jpeg' alt="bottle of water" />
       </div>
     </section>
