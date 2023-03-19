0cb52cf21d36b16992fb4ee47fc3b1c01b945a85
matt-pilcher <mpilcher90@gmail.com>
2023-03-19 18:09:28 -0400

    update app description

--- components/UploadWrapper.js
+++ components/UploadWrapper.js
@@ -8,7 +8,7 @@ function UploadWrapper({ uploadButtonElement }) {
       <section className="flex mx-auto flex-col md:flex-row justify-between max-w-7xl p-6">
         <div className="justify-center flex flex-col md:w-1/2">
           <h1 className="font-bold leading-tight text-gray-900 lg:text-6xl md:text-5xl text-3xl mb-5">AI-Powered Background Image Removal</h1>
-          <p className="font-medium mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <p className="font-medium mb-5">Upload an image to remove the background and make it transparent. This app uses AI to detect the image background so accurate results are not guaranteed.</p>
           <div className="my-4">
             {uploadButtonElement("Upload Image")}
           </div>
